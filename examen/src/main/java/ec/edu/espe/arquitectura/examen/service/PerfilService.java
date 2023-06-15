4b14f87df6b11fed7fd09397bc30e96f9d4b2b0f
Jerry Tovar <65546801+joetovar@users.noreply.github.com>
2023-06-14 21:22:39 -0500

    Update services
    by JJ

--- examen/src/main/java/ec/edu/espe/arquitectura/examen/service/PerfilService.java
+++ examen/src/main/java/ec/edu/espe/arquitectura/examen/service/PerfilService.java
@@ -1,14 +1,24 @@
 package ec.edu.espe.arquitectura.examen.service;
 
-import java.util.Date;
+
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.stereotype.Service;
 
 import ec.edu.espe.arquitectura.examen.model.SegPerfil;
 import ec.edu.espe.arquitectura.examen.repository.SegPerfilRepository;
 
+
+@Service
 public class PerfilService {
     
+     private final SegPerfilRepository segPerfilRepository;
+
+    public PerfilService(SegPerfilRepository segPerfilRepository) {
+        this.segPerfilRepository = segPerfilRepository;
+    }
+    public List<SegPerfil> listByNombre(String nombre) {
+        return this.segPerfilRepository.findByNombreOrderByNombre(nombre);
+    }
 }
