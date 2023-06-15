4ac8ef2bd25b747a810d9cca10edc47632cca41a
Jerry Tovar <65546801+joetovar@users.noreply.github.com>
2023-06-14 20:01:02 -0500

    class SegUsuario
    by Jerry Tovar

--- examen/src/main/java/ec/edu/espe/arquitectura/examen/model/SegUsuario.java
+++ examen/src/main/java/ec/edu/espe/arquitectura/examen/model/SegUsuario.java
@@ -1,5 +1,142 @@
 package ec.edu.espe.arquitectura.examen.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
+@Entity
+@Table(name = "SEG_USUARIO")
 public class SegUsuario {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "COD_USUARIO", length = 8, nullable = false)
+    private Integer codigo;
+    @Column(name = "MAIL", length = 128, nullable = false)
+    private String mail;
+    @Column(name = "CLAVE", length = 64, nullable = false)
+    private String clave;
+    @Column(name = "NOMBRE", length = 128, nullable = false)
+    private String nombre;
+    @Column(name = "INTENTOS_FALLIDOS", length = 8, nullable = false)
+    private Integer intentosFallidos;
+    @Column(name = "SUELDO", scale = 8, precision = 2, nullable = false)
+    private BigDecimal sueldo;
+    @Column(name = "FECHA_NACIMIENTO", nullable = false)
+    private Date fechaNacimiento;
+    @Version
+    @Column(name = "VERSION", nullable = false)
+    private Integer version;
+
+    public SegUsuario() {
+    }
+
+    public SegUsuario(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIntentosFallidos() {
+        return intentosFallidos;
+    }
+
+    public void setIntentosFallidos(Integer intentosFallidos) {
+        this.intentosFallidos = intentosFallidos;
+    }
+
+    public BigDecimal getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(BigDecimal sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SegUsuario other = (SegUsuario) obj;
+        if (codigo == null) {
+            if (other.codigo != null)
+                return false;
+        } else if (!codigo.equals(other.codigo))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "SegUsuario [codigo=" + codigo + ", mail=" + mail + ", clave=" + clave + ", nombre=" + nombre
+                + ", intentosFallidos=" + intentosFallidos + ", sueldo=" + sueldo + ", fechaNacimiento="
+                + fechaNacimiento + ", version=" + version + "]";
+    }
+
+
+
 }
